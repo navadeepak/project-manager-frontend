f3567e021f4f68766af85624f33b6f3079ecdf64
navadeepak <navadeepakaswin007@gmail.com>
2025-06-06 17:33:54 +0530

    multer finished

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -1,5 +1,7 @@
 import { Icon } from "@iconify/react/dist/iconify.js";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import axiosInstance from "../utilities/AxiosInstance";
+import { toast, Toaster } from "sonner";
 
 const dummyData = [
   {
@@ -35,8 +37,75 @@ const dummyData = [
 
 function Home() {
   const [addProjectModal, setAddProjectModal] = useState(false);
+  const [formData, setFormData] = useState({});
+  const [projects, setProjects] = useState([]);
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleFileChange = (e) => {
+    setFormData({
+      ...formData,
+      image: e.target.files[0],
+    });
+  };
+
+  const getAllProjects = async () => {
+    try {
+      const response = await axiosInstance.get("/projects/get");
+      console.log("Projects:", response);
+      setProjects(response.data);
+    } catch (error) {
+      console.error("Error fetching projects:", error);
+      return [];
+    }
+  };
+
+  useEffect(() => {
+    getAllProjects();
+  }, []);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const formDataToSend = new FormData();
+      Object.entries(formData).forEach(([key, value]) => {
+        if (key === "tech_stack") {
+          formDataToSend.append(
+            key,
+            value.split(",").map((item) => item.trim())
+          );
+        } else {
+          formDataToSend.append(key, value);
+        }
+      });
+      const response = await axiosInstance.post(
+        "/projects/create",
+        formDataToSend,
+        {
+          headers: {
+            "Content-Type": "multipart/form-data",
+          },
+        }
+      );
+      console.log("Response:", response.data);
+      toast.success("Project Added Successfully");
+      setAddProjectModal(false);
+      setFormData({});
+    } catch (error) {
+      console.error("Error:", error);
+      toast.error("Error Adding Project");
+    }
+  };
+
   return (
     <div className="w-full h-full flex flex-col gap-5 px-4">
+      <Toaster position="top-center" />
       <h1 className="font-semibold text-gray-700 text-4xl">Projects:</h1>
       <div className="flex flex-row flex-wrap items-center gap-4">
         {location.pathname === "/admin" && (
@@ -50,9 +119,9 @@ function Home() {
             </h2>
           </div>
         )}
-        {dummyData.map((project) => (
+        {projects.map((project, index) => (
           <div
-            key={project.id}
+            key={index}
             className="flex flex-row items-center justify-center p-4 text-white cursor-pointer bg-blue-500 w-[425px] h-60 rounded gap-4"
           >
             <div>
@@ -64,8 +133,10 @@ function Home() {
               <p className="mt-2">
                 Tech Stack:{" "}
                 <span className="font-semibold">
-                  {project.tech_stack.join(", ")}
-                </span>
+                  {Array.isArray(project?.tech_stack)
+                    ? project.tech_stack.join(", ")
+                    : project?.tech_stack}
+                </span>{" "}
               </p>
             </div>
             <div className="flex flex-col group bg-[#ffffff30] overflow-hidden rounded-md h-52 w-full items-center justify-center relative">
@@ -75,12 +146,22 @@ function Home() {
                 className="w-24 rounded absolute group-hover:hidden flex"
               />
               <div className="hidden flex-col group-hover:flex gap-2 w-44 text-nowrap px-4">
-                <button className="bg-[#00000050] text-white p-2 rounded w-full">
+                <a
+                  href={project.repo_link}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="bg-[#00000050] text-white p-2 rounded w-full text-center"
+                >
                   Repo Link
-                </button>
-                <button className="bg-[#00000050] text-white p-2 rounded w-full">
+                </a>
+                <a
+                  href={project.live_link}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="bg-[#00000050] text-white p-2 rounded w-full text-center"
+                >
                   Live Link
-                </button>
+                </a>
                 {location.pathname === "/admin" && (
                   <button className="bg-[#00000050] text-white p-2 rounded w-full">
                     Edit Project
@@ -103,36 +184,58 @@ function Home() {
                 <Icon icon="line-md:close-small" width="24" height="24" />
               </button>
             </div>
-            {/* Form for adding a new project */}
-            <form>
+            <form onSubmit={handleSubmit}>
               <input
                 type="text"
                 placeholder="Project Name"
+                name="name"
+                value={formData.name}
+                onChange={handleInputChange}
                 className="w-full p-2 mb-4 border rounded"
+                required
               />
               <textarea
                 placeholder="Project Description"
+                name="description"
+                value={formData.description}
+                onChange={handleInputChange}
                 className="w-full p-2 mb-4 border rounded"
+                required
               ></textarea>
               <input
                 type="text"
                 placeholder="Tech Stack (comma separated)"
+                name="tech_stack"
+                value={formData.tech_stack}
+                onChange={handleInputChange}
                 className="w-full p-2 mb-4 border rounded"
+                required
               />
               <input
-                type="text"
+                type="url"
                 placeholder="Repository Link"
+                name="repo_link"
+                value={formData.repo_link}
+                onChange={handleInputChange}
                 className="w-full p-2 mb-4 border rounded"
+                required
               />
               <input
-                type="text"
+                type="url"
                 placeholder="Live Link"
+                name="live_link"
+                value={formData.live_link}
+                onChange={handleInputChange}
                 className="w-full p-2 mb-4 border rounded"
+                required
               />
               <input
                 type="file"
-                placeholder="Project Image"
+                name="image"
+                onChange={handleFileChange}
+                accept="image/*"
                 className="w-full p-2 mb-4 border rounded"
+                required
               />
               <button
                 type="submit"
